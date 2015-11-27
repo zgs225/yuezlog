c06a20079f8958809a9fb924b9b364513e0d04c6
zgs225 <zgs225@gmail.com>
2015-11-27 11:54:42 +0800

    完成练习1.11

--- source/downloads/code/sicp/ex1-11.scm
+++ source/downloads/code/sicp/ex1-11.scm
@@ -1,6 +1,6 @@
 ; 使用递归计算过程
 (define (f n)
-  (if (< n 3) 1
+  (if (< n 3) n
       (+ (f (- n 1))
          (* 2 (f (- n 2)))
          (* 3 (f (- n 3))))))
@@ -8,9 +8,7 @@
 ; 使用迭代计算过程
 (define (f2 n)
   (define (f2-iter x y z count)
-    (if (< count 3) x
-        (f2-iter (+ x (* 2 y) (* 3 z))
-                 x
-                 y
-                 (- count 1))))
-  (f2-iter 1 1 1 n))
+    (cond ((< count 2) count)
+          ((= count 2) x)
+          (else (f2-iter (+ x (* 2 y) (* 3 z)) x y (- count 1)))))
+  (f2-iter 2 1 0 n))
