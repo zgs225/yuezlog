5d13ae98dc5f068e70c090334f1634c902227e8c
zgs225 <zgs225@gmail.com>
2016-01-28 08:22:20 +0800

    完成练习1.46

---
+++ source/downloads/code/sicp/ex1-46.scm
@@ -0,0 +1,8 @@
+(define (iterative-improve good-enough? improve)
+  (lambda (first-guess)
+    (define (try guess)
+       (let ((next (improve guess)))
+         (if (good-enough? guess next)
+             next
+             (try next))))
+     (try first-guess)))
