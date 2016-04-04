be031a19b56c955448d880952b7e73aac45ed73f
zgs225 <zgs225@gmail.com>
2016-04-05 07:46:38 +0800

    完成练习2.38

---
+++ source/downloads/code/sicp/p82-fold-left.scm
@@ -0,0 +1,9 @@
+;; Happy hacking Yuez - Emacs ♥ you!
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
