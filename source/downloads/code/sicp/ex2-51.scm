2ec8e341820ac42e95a0e9250bac1500fb18a7ec
zgs225 <zgs225@gmail.com>
2016-04-29 07:41:10 +0800

    完成练习2.51

---
+++ source/downloads/code/sicp/ex2-51.scm
@@ -0,0 +1,19 @@
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0 0.5)))
+    (let ((paint-top
+            (transform-painter painter2
+                               split-point
+                               (make-vect 1.0 0)
+                               (make-vect 0 1.0)))
+          ((paint-bottom
+             (transform-painter painter1
+                                (make-vect 0 0)
+                                (make-vect 1.0 0)
+                                split-point))))
+      (lambda (frame)
+        (paint-top frame)
+        (paint-bottom frame)))))
+
+(define (below2 painter1 painter2)
+  (beside (rotate90 painter1)
+          (rotate270 painter2)))
