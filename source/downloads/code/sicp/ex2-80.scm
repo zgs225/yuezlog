11bf1059a9e6f8d106920d7ce56e474a2125b411
zgs225 <zgs225@gmail.com>
2016-06-03 07:24:06 +0800

    完成练习2.80

---
+++ source/downloads/code/sicp/ex2-80.scm
@@ -0,0 +1,11 @@
+(define (=zero? x) (apply-generic '=zero? x))
+
+(define (install-=zero?-package)
+  (put '=zero? '(scheme-number)
+       (lambda (x) (= x 0)))
+  (put '=zero? '(rational)
+       (lambda (x) (= (numer x) 0)))
+  (put '=zero? '(complex)
+       (lambda (x) (and (= (real-part x) 0)
+                        (= (imag-part x) 0))))
+  'done)
