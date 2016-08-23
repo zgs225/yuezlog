b1e2f967863350b6119ee0533c7ae19b3277c29c
zgs225 <zgs225@gmail.com>
2016-08-24 06:16:27 +0800

    完成练习3.59

--- source/downloads/code/sicp/ex3-59.scm
+++ source/downloads/code/sicp/ex3-59.scm
@@ -3,3 +3,12 @@
                       (* a (/ 1 n)))
               stream
               integers))
+
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+
+(define cosine-series
+  (cons-stream 1 (stream-map - (integrate-series sine-series))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
