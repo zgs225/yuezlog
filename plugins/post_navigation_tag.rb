220d2329620baee262b25c60c62df5737330657f
zgs225 <zgs225@gmail.com>
2020-05-13 15:41:07 +0800

    添加Google Adsense 配置

--- plugins/post_navigation_tag.rb
+++ plugins/post_navigation_tag.rb
@@ -1,6 +1,4 @@
 # encoding: utf-8
-require 'byebug'
-
 module Jekyll
   class PostNavigationTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
@@ -8,7 +6,6 @@ module Jekyll
     end
 
     def render(context)
-      byebug
       "<h1>Hello world</h1>"
     end
   end
